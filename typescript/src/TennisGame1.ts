6591773c62ece801b315bd6bfc69cdeb8bbfb013
Johan Martinsson <j.martinsson@bee-buzziness.com>
2018-05-14 00:41:39 +0200

    got rid of the bug, the duplication and reextracted advantageOrWin this time to a class method

--- typescript/src/TennisGame1.ts
+++ typescript/src/TennisGame1.ts
@@ -1,5 +1,44 @@
 import {TennisGame} from './TennisGame';
 
+export class TennisGame1 implements TennisGame {
+    private m_score1: number = 0;
+    private m_score2: number = 0;
+    private player1Name: string;
+    private player2Name: string;
+
+    constructor(player1Name: string, player2Name: string) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
+    }
+
+    wonPoint(playerName: string): void {
+        if (playerName === this.player1Name)
+            this.m_score1 += 1;
+        else
+            this.m_score2 += 1;
+    }
+
+    getScore(): string {
+        let p1Score = this.m_score1;
+        let p2Score = this.m_score2;
+        if (p1Score === p2Score) {
+            return equalityScore(p1Score);
+        }
+        else if (p1Score >= 4 || p2Score >= 4) {
+            return this.advantageOrWin(p1Score, p2Score);
+        }
+        else {
+            return pointScore(p1Score, p2Score);
+        }
+    }
+
+    private advantageOrWin(p1Score, p2Score) {
+        const minusResult: number = p1Score - p2Score;
+        const leader = minusResult > 0 ? this.player1Name : this.player2Name;
+        let typeOfScore = Math.abs(minusResult) === 1 ? 'Advantage ' : 'Win for ';
+        return typeOfScore + leader;
+    }
+}
 
 function pointScore(p1Score, p2Score) {
     const scoreTitles = {
@@ -20,40 +59,3 @@ function equalityScore(p1Score) {
     return sameScores[p1Score] || "Deuce";
 }
 
-export class TennisGame1 implements TennisGame {
-    private m_score1: number = 0;
-    private m_score2: number = 0;
-    private player1Name: string;
-    private player2Name: string;
-
-    constructor(player1Name: string, player2Name: string) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
-    }
-
-    wonPoint(playerName: string): void {
-        // TODO BUG a game cannot be played if the first player isn't called "player1"!
-        if (playerName === 'player1')
-            this.m_score1 += 1;
-        else
-            this.m_score2 += 1;
-    }
-
-    getScore(): string {
-        let p1Score = this.m_score1;
-        let p2Score = this.m_score2;
-        if (p1Score === p2Score) {
-            return equalityScore(p1Score);
-        }
-        else if (p1Score >= 4 || p2Score >= 4) {
-            const minusResult: number = p1Score - p2Score;
-            const leader = minusResult > 0 ? this.player1Name : this.player2Name;
-            let typeOfScore = Math.abs(minusResult) === 1 ? 'Advantage ' : 'Win for ';
-            let s = typeOfScore + leader;
-            return s;
-        }
-        else {
-            return pointScore(p1Score, p2Score);
-        }
-    }
-}
