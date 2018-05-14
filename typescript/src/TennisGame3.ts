5a3c9a87f5391006293b6e933a1a051e7242cb21
Johan Martinsson <j.martinsson@bee-buzziness.com>
2018-05-14 02:41:20 +0200

    TS: step 2 meaningful variables uncouple by doing lookups more than once remove duplication

--- typescript/src/TennisGame3.ts
+++ typescript/src/TennisGame3.ts
@@ -1,36 +1,38 @@
-import { TennisGame } from './TennisGame';
+import {TennisGame} from './TennisGame';
 
+const pointTitles: string[] = ['Love', 'Fifteen', 'Thirty', 'Forty'];
 
 export class TennisGame3 implements TennisGame {
-  private p2: number = 0;
-  private p1: number = 0;
-  private p1N: string;
-  private p2N: string;
+    private p2: number = 0;
+    private p1: number = 0;
+    private p1N: string;
+    private p2N: string;
 
-  constructor(p1N: string, p2N: string) {
-    this.p1N = p1N;
-    this.p2N = p2N;
-  }
-
-  getScore(): string {
-    let s: string;
-    if (this.p1 < 4 && this.p2 < 4 && !(this.p1 + this.p2 === 6)) {
-      const p: string[] = ['Love', 'Fifteen', 'Thirty', 'Forty'];
-      s = p[this.p1];
-      return (this.p1 === this.p2) ? s + '-All' : s + '-' + p[this.p2];
-    } else {
-      if (this.p1 === this.p2)
-        return 'Deuce';
-      s = this.p1 > this.p2 ? this.p1N : this.p2N;
-      return (((this.p1 - this.p2) * (this.p1 - this.p2)) === 1) ? 'Advantage ' + s : 'Win for ' + s;
+    constructor(p1N: string, p2N: string) {
+        this.p1N = p1N;
+        this.p2N = p2N;
     }
-  }
 
-  wonPoint(playerName: string): void {
-    if (playerName === 'player1')
-      this.p1 += 1;
-    else
-      this.p2 += 1;
+    getScore(): string {
+        let beforeDeuce = (this.p1 + this.p2 < 6);
+        let noOneHasWonYet = this.p1 < 4 && this.p2 < 4;
+        let equality = this.p1 === this.p2;
+        if (noOneHasWonYet && beforeDeuce) {
+            return equality ? pointTitles[this.p1] + '-All' : pointTitles[this.p1] + '-' + pointTitles[this.p2];
+        } else {
+            if (equality)
+                return 'Deuce';
+            let leaderName: string = this.p1 > this.p2 ? this.p1N : this.p2N;
+            let gameHasWinner = Math.abs(this.p1 - this.p2) > 1;
+            return (gameHasWinner ? 'Win for ' : 'Advantage ') + leaderName;
+        }
+    }
 
-  }
+    wonPoint(playerName: string): void {
+        if (playerName === 'player1')
+            this.p1 += 1;
+        else
+            this.p2 += 1;
+
+    }
 }
