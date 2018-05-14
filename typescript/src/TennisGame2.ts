3dab6dc228151cda7ae3870f75098ead1475c085
Johan Martinsson <j.martinsson@bee-buzziness.com>
2018-05-14 12:21:55 +0200

    poor mans pattern matching, no ifs in TS

--- typescript/src/TennisGame2.ts
+++ typescript/src/TennisGame2.ts
@@ -1,126 +1,89 @@
-import { TennisGame } from './TennisGame';
+import {TennisGame} from './TennisGame';
+import * as _ from 'lodash';
 
+const pointTitles: string[] = ['Love', 'Fifteen', 'Thirty', 'Forty'];
+
+let equalPointScore = {
+    matches: (P1point, P2point) => P1point === P2point && P1point < 4,
+    showScore: (P1point, P2point) => pointTitles[P1point] + "-All"
+};
+let deuce = {
+    matches: (P1point, P2point) => P1point === P2point && P1point >= 3 && P2point >= 3,
+    showScore: (P1point, P2point) => "Deuce"
+};
+let normalPointScore = {
+    matches: (P1point, P2point) => P1point < 4 || P2point < 4,
+    showScore: (P1point, P2point) => pointTitles[P1point] + "-" + pointTitles[P2point]
+};
+let winScore = {
+    matches: (P1point, P2point) => (P1point - P2point) >= 2,
+    showScore: (P1point, P2point) => "Win for " + winnerName(P1point, P2point)
+};
+let advantageScore = {
+    matches: (P1point, P2point) => (P1point - P2point) ===1,
+    showScore: (P1point, P2point) => "Advantage for " + winnerName(P1point, P2point)
+};
+const scoreTypes: any[] = [
+    deuce,
+    equalPointScore,
+    normalPointScore,
+    advantageScore,
+
+];
+
+function winnerName(P1point, P2point) {
+    return P1point > P2point ? 'player1' : "player2"
+}
 
 export class TennisGame2 implements TennisGame {
-  P1point: number = 0;
-  P2point: number = 0;
+    P1point: number = 0;
+    P2point: number = 0;
 
-  P1res: string = '';
-  P2res: string = '';
+    P1res: string = '';
+    P2res: string = '';
 
-  private player1Name: string;
-  private player2Name: string;
+    private player1Name: string;
+    private player2Name: string;
 
-  constructor(player1Name: string, player2Name: string) {
-    this.player1Name = player1Name;
-    this.player2Name = player2Name;
-  }
-
-  getScore(): string {
-    let score: string = '';
-    if (this.P1point === this.P2point && this.P1point < 4) {
-      if (this.P1point === 0)
-        score = 'Love';
-      if (this.P1point === 1)
-        score = 'Fifteen';
-      if (this.P1point === 2)
-        score = 'Thirty';
-      score += '-All';
-    }
-    if (this.P1point === this.P2point && this.P1point >= 3)
-      score = 'Deuce';
-
-    if (this.P1point > 0 && this.P2point === 0) {
-      if (this.P1point === 1)
-        this.P1res = 'Fifteen';
-      if (this.P1point === 2)
-        this.P1res = 'Thirty';
-      if (this.P1point === 3)
-        this.P1res = 'Forty';
-
-      this.P2res = 'Love';
-      score = this.P1res + '-' + this.P2res;
-    }
-    if (this.P2point > 0 && this.P1point === 0) {
-      if (this.P2point === 1)
-        this.P2res = 'Fifteen';
-      if (this.P2point === 2)
-        this.P2res = 'Thirty';
-      if (this.P2point === 3)
-        this.P2res = 'Forty';
-
-      this.P1res = 'Love';
-      score = this.P1res + '-' + this.P2res;
+    constructor(player1Name: string, player2Name: string) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
     }
 
-    if (this.P1point > this.P2point && this.P1point < 4) {
-      if (this.P1point === 2)
-        this.P1res = 'Thirty';
-      if (this.P1point === 3)
-        this.P1res = 'Forty';
-      if (this.P2point === 1)
-        this.P2res = 'Fifteen';
-      if (this.P2point === 2)
-        this.P2res = 'Thirty';
-      score = this.P1res + '-' + this.P2res;
-    }
-    if (this.P2point > this.P1point && this.P2point < 4) {
-      if (this.P2point === 2)
-        this.P2res = 'Thirty';
-      if (this.P2point === 3)
-        this.P2res = 'Forty';
-      if (this.P1point === 1)
-        this.P1res = 'Fifteen';
-      if (this.P1point === 2)
-        this.P1res = 'Thirty';
-      score = this.P1res + '-' + this.P2res;
+    getScore(): string {
+        let foundType = _.find(scoreTypes, scoreType => scoreType.matches(this.P1point, this.P2point))
+            || winScore
+        return foundType.showScore(this.P1point, this.P2point);
     }
 
-    if (this.P1point > this.P2point && this.P2point >= 3) {
-      score = 'Advantage player1';
+    SetP1Score(score: number): void {
+
+        for (let i = 0; i < score; i++) {
+            this.P1Score();
+        }
+
     }
 
-    if (this.P2point > this.P1point && this.P1point >= 3) {
-      score = 'Advantage player2';
+    SetP2Score(score: number): void {
+
+        for (let i = 0; i < score; i++) {
+            this.P2Score();
+        }
+
     }
 
-    if (this.P1point >= 4 && this.P2point >= 0 && (this.P1point - this.P2point) >= 2) {
-      score = 'Win for player1';
-    }
-    if (this.P2point >= 4 && this.P1point >= 0 && (this.P2point - this.P1point) >= 2) {
-      score = 'Win for player2';
-    }
-    return score;
-  }
-
-  SetP1Score(score: number): void {
-
-    for (let i = 0; i < score; i++) {
-      this.P1Score();
+    P1Score(): void {
+        this.P1point++;
     }
 
-  }
-
-  SetP2Score(score: number): void {
-
-    for (let i = 0; i < score; i++) {
-      this.P2Score();
+    P2Score(): void {
+        this.P2point++;
     }
 
-  }
-
-  P1Score(): void {
-    this.P1point++;
-  }
-
-  P2Score(): void {
-    this.P2point++;
-  }
-
-  wonPoint(player: string): void {
-    if (player === 'player1')
-      this.P1Score();
-    else
-      this.P2Score();
-  }
+    wonPoint(player: string): void {
+        if (player === 'player1')
+            this.P1Score();
+        else
+            this.P2Score();
+    }
 }
