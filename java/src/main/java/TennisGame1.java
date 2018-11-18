f5b8e44b4b4064495f9170add9fb854c054b359d
Johan Martinsson <j.martinsson@bee-buzziness.com>
2018-11-19 00:09:05 +0100

    tried a TreeMap for the first solution

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,10 +1,17 @@
+import java.util.*;
 
 public class TennisGame1 implements TennisGame {
-    
+
     private int m_score1 = 0;
     private int m_score2 = 0;
     private String player1Name;
     private String player2Name;
+    private Map<Integer, String> scoreNames = new HashMap<Integer, String>() {{
+        put(0, "Love");
+        put(1, "Fifteen");
+        put(2, "Thirty");
+        put(3, "Forty");
+    }};
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -20,56 +27,29 @@ public class TennisGame1 implements TennisGame {
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+        int tempScore = 0;
+        if (m_score1 == m_score2 && m_score1 + m_score1 <= 4) {
+            score = this.scoreNames.get(m_score1) + "-All";
+        } else if (m_score1 >= 4 || m_score2 >= 4 || m_score1 == m_score2) {
+            List<String> ployoffScores = Arrays.asList("Win for player2", "Advantage player2", "Deuce", "Advantage player1", "Win for player1");
+            TreeMap<Integer, String> poScores = new TreeMap<Integer, String>() {{
+                put(-2, "Win for player2");
+                put(-1, "Advantage player2");
+                put(0, "Deuce");
+                put(1, "Advantage player1");
+                put(2, "Win for player1");
+            }};
+            int diff = m_score1 - m_score2;
+            int min = Math.min(2, Math.max(-2, diff));
+            score = ployoffScores.get(min + 2);
+//            int minusResult = m_score1 - m_score2;
+//            if (minusResult == 0) score = "Deuce";
+//            else if (minusResult == 1) score = "Advantage player1";
+//            else if (minusResult == -1) score = "Advantage player2";
+//            else if (minusResult >= 2) score = "Win for player1";
+//            else score = "Win for player2";
+        } else {
+            score = this.scoreNames.get(m_score1) + "-" + this.scoreNames.get(m_score2);
         }
         return score;
     }
