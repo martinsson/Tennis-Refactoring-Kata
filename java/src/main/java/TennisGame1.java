186f51f76e565890eaba64acdf607ed55c2285a7
Johan Martinsson <j.martinsson@bee-buzziness.com>
2018-11-19 00:12:40 +0100

    put the playoffscores as a field in the class

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -12,6 +12,14 @@ public class TennisGame1 implements TennisGame {
         put(2, "Thirty");
         put(3, "Forty");
     }};
+    private TreeMap<Integer, String> playoffScores = new TreeMap<Integer, String>() {{
+        put(-2, "Win for player2");
+        put(-1, "Advantage player2");
+        put(0, "Deuce");
+        put(1, "Advantage player1");
+        put(2, "Win for player1");
+    }};
+
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -27,27 +35,12 @@ public class TennisGame1 implements TennisGame {
 
     public String getScore() {
         String score = "";
-        int tempScore = 0;
         if (m_score1 == m_score2 && m_score1 + m_score1 <= 4) {
             score = this.scoreNames.get(m_score1) + "-All";
         } else if (m_score1 >= 4 || m_score2 >= 4 || m_score1 == m_score2) {
-            List<String> ployoffScores = Arrays.asList("Win for player2", "Advantage player2", "Deuce", "Advantage player1", "Win for player1");
-            TreeMap<Integer, String> poScores = new TreeMap<Integer, String>() {{
-                put(-2, "Win for player2");
-                put(-1, "Advantage player2");
-                put(0, "Deuce");
-                put(1, "Advantage player1");
-                put(2, "Win for player1");
-            }};
             int diff = m_score1 - m_score2;
             int min = Math.min(2, Math.max(-2, diff));
-            score = ployoffScores.get(min + 2);
-//            int minusResult = m_score1 - m_score2;
-//            if (minusResult == 0) score = "Deuce";
-//            else if (minusResult == 1) score = "Advantage player1";
-//            else if (minusResult == -1) score = "Advantage player2";
-//            else if (minusResult >= 2) score = "Win for player1";
-//            else score = "Win for player2";
+            score = playoffScores.get(min);
         } else {
             score = this.scoreNames.get(m_score1) + "-" + this.scoreNames.get(m_score2);
         }
