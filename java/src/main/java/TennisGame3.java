63a8f00e760de19a081745e692de6b0a13c926d8
Johan Martinsson <j.martinsson@bee-buzziness.com>
2018-11-22 22:21:09 +0100

    refactor TennisGame3

--- java/src/main/java/TennisGame3.java
+++ java/src/main/java/TennisGame3.java
@@ -1,36 +1,51 @@
 
 public class TennisGame3 implements TennisGame {
-    
-    private int p2;
-    private int p1;
-    private String p1N;
-    private String p2N;
+
+    private int p2Score;
+    private int p1Score;
+    private String p1Name;
+    private String p2Name;
+    private String[] pointScores = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
 
     public TennisGame3(String p1N, String p2N) {
-        this.p1N = p1N;
-        this.p2N = p2N;
+        this.p1Name = p1N;
+        this.p2Name = p2N;
     }
 
     public String getScore() {
-        String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; 
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
-        } else {
-            if (p1 == p2)
+        if (isInPlayoff()) {
+            if (p1Score == p2Score)
                 return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+            else if (Math.abs(p1Score - p2Score) == 1)
+                return "Advantage " + leadingPlayerName();
+            else
+                return "Win for " + leadingPlayerName();
+        } else {
+            if (p1Score == p2Score)
+                return pointScore(p1Score) + "-All";
+            else
+                return pointScore(p1Score) + "-" + pointScore(p2Score);
         }
     }
-    
+
+    private String pointScore(int intScore) {
+        return pointScores[intScore];
+    }
+
+    private String leadingPlayerName() {
+        return p1Score > p2Score ? p1Name : p2Name;
+    }
+
+    private boolean isInPlayoff() {
+        return p1Score >= 4 || p2Score >= 4 || p1Score + p2Score == 6;
+    }
+
     public void wonPoint(String playerName) {
         if (playerName == "player1")
-            this.p1 += 1;
+            this.p1Score += 1;
         else
-            this.p2 += 1;
-        
+            this.p2Score += 1;
+
     }
 
 }
